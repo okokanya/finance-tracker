c2dd50cc5cadb9856ccdf3caa42d74d77b994890
Vyacheslav Bogdanov <slavabogdn@gmail.com>
2025-02-03 20:45:22 +0700

    fix: layout max width

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -6,7 +6,7 @@ const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className="flex h-[100vh] flex-col">
       <Header />
-      <main className="flex w-full max-w-[1440px] flex-grow columns-4 flex-col items-center gap-5 p-5 md:mx-auto md:my-0 md:columns-12 md:p-0">
+      <main className="flex w-full max-w-[1180px] flex-grow columns-4 flex-col items-center gap-5 p-5 md:mx-auto md:my-0 md:columns-12 md:p-0">
         {children}
       </main>
     </div>
