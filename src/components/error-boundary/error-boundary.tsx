d0948fdf8fcb4d3d8667ca2d9ce8efc3b15f204a
Vyacheslav Bogdanov <slavabogdn@gmail.com>
2025-02-18 21:59:54 +0700

    feat: Error Boundary component

---
+++ src/components/error-boundary/error-boundary.tsx
@@ -0,0 +1,59 @@
+import React, { ErrorInfo, PropsWithChildren } from 'react';
+
+import { cn } from '@/utils/cn';
+
+import Button from '../button';
+import Text from '../text/text';
+import Title from '../title/title';
+
+type ErrorBoundaryProps = PropsWithChildren<{
+  error?: Error;
+}>;
+
+type ErrorBoundaryState = {
+  hasError: boolean;
+};
+
+class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+
+    this.state = { hasError: false };
+  }
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.log({ error, errorInfo });
+  }
+
+  handleClick = () => {
+    this.setState({ hasError: false });
+    window.location.reload();
+  };
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div
+          className={cn('mx-auto flex h-[100dvh] max-w-[1180px] items-center justify-center py-5')}
+        >
+          <div className={cn('flex max-w-[400px] flex-col gap-2')}>
+            <div className={cn('items-self-start flex flex-col gap-2')}>
+              <Title>Что-то пошло не так</Title>
+              <Text isBold>Попробуйте перезагрузить страницу</Text>
+            </div>
+
+            <Button className={cn('self-start')} onClick={this.handleClick}>
+              Перезагрузить
+            </Button>
+          </div>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,28 @@
 import '@/styles/globals.css';
 
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import Head from 'next/head';
 
+import ErrorBoundary from '@/components/error-boundary/error-boundary';
 import Layout from '@/components/layout/layout';
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <ErrorBoundary>
+      <QueryClientProvider client={queryClient}>
+        <Head>
+          <title>Finance Tracker || {Component.title}</title>
+        </Head>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
 
-    <QueryClientProvider client={queryClient}>
-      <Head>
-       <title>Finance Tracker || {Component.title}</title>
-      </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ErrorBoundary>
   );
 }
