bd99d7ff60252bc0fe992dcd0e549ff9b0ec7104
anya pushkina <okokanya@gmail.com>
2025-02-18 12:49:25 +0800

    db connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,17 @@
-import { drizzle } from 'drizzle-orm/libsql';
-const db = drizzle({ connection: {
-  url: process.env.DATABASE_URL,
-  authToken: process.env.DATABASE_AUTH_TOKEN
-}});
+import { drizzle, Client } from 'drizzle-orm/libsql';
+
+// Типизация параметров для подключения
+interface ConnectionConfig {
+  url: string | undefined;
+  authToken: string | undefined;
+}
+
+// Подключение к базе данных
+const db = drizzle({
+  client: {
+    url: process.env.DATABASE_URL,
+    authToken: process.env.DATABASE_AUTH_TOKEN,
+  } as Client,
+});
+
+export default db;
