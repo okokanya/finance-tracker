68cf8fac625f28c4058b2aa5b91ae567ed1c35f4
anya pushkina <okokanya@gmail.com>
2025-03-05 12:24:07 +0800

    conflicts resolve

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import '@/styles/globals.css';
 
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import Head from 'next/head';
 
+import ErrorBoundary from '@/components/error-boundary/error-boundary';
 import Layout from '@/components/layout/layout';
+
 type CustomAppProps = AppProps & {
   Component: AppProps['Component'] & {
     title?: string;
@@ -16,16 +18,16 @@ const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: CustomAppProps) {
   return (
-
-    <QueryClientProvider client={queryClient}>
-      <Head>
-       <title>{`Finance Tracker - ${Component.title}`}</title>
-      </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <ErrorBoundary>
+      <QueryClientProvider client={queryClient}>
+        <Head>
+          <title>{`Finance Tracker - ${Component.title}`}</title>
+        </Head>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ErrorBoundary>
   );
 }
