38655961c3311d66e31f8b0919f24b9f412d9fc2
Vyacheslav Bogdanov <slavabogdn@gmail.com>
2025-03-18 21:53:41 +0700

    lint errors

--- src/components/form-wrap.tsx
+++ src/components/form-wrap.tsx
@@ -7,13 +7,11 @@ type Props = PropsWithChildren & {
 const FormWrap = ({ children, width }: Props) => {
   return (
     <div
-      className="rounded-[12px] bg-transparent px-8 py-8 sm:bg-white w-full sm:w-[24rem]"
+      className="w-full rounded-[12px] bg-transparent px-8 py-8 sm:w-[24rem] sm:bg-white"
       style={{ width }}
     >
       {children && (
-        <div className="flex h-full w-full flex-col items-center justify-center">
-          {children}
-        </div>
+        <div className="flex h-full w-full flex-col items-center justify-center">{children}</div>
       )}
     </div>
   );
