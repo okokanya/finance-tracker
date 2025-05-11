9bc3e75da7eefe14d7db9a860e062672853a720b
anya pushkina <okokanya@gmail.com>
2025-05-11 11:22:01 +0800

    fix margins on signup page

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -96,21 +96,26 @@ export default function Signup() {
         </Title>
         <form className="flex w-full flex-wrap justify-between" onSubmit={handleSubmit(onSubmit)}>
           <Input
-            wrapperClassName="w-full sm:w-1/2"
+            wrapperClassName="w-full sm:w-1/2 mb-4 box-border"
+            className="w-[calc(100%-16px)]"
             label="Имя"
             placeholder="Ярополк"
             {...register('firstName')}
             errorText={errors.firstName?.message}
           />
           <Input
-            wrapperClassName="w-full sm:w-1/2"
+            wrapperClassName="w-full sm:w-1/2 mb-4 box-border"
+            className="w-[calc(100%-16px)]"
             label="Фамилия"
             placeholder="Иванов"
             {...register('lastName')}
             errorText={errors.lastName?.message}
           />
           <Input
-            wrapperClassName="w-full"
+            wrapperClassName="w-full mb-4 max-w-full"
+            className="w-[calc(100%-16px)]"
+
+            // className="w-full"
             label="Email"
             type="email"
             placeholder="ivanov@ya.com"
@@ -118,7 +123,8 @@ export default function Signup() {
             errorText={errors.email?.message}
           />
           <Input
-            wrapperClassName="w-full sm:w-1/2"
+            wrapperClassName="w-full sm:w-1/2 mb-4"
+            className="w-[calc(100%-16px)]"
             label="Придумайте пароль"
             type="password"
             placeholder="******"
@@ -127,6 +133,8 @@ export default function Signup() {
           />
           <Input
             wrapperClassName="w-full sm:w-1/2"
+            // className="w-full"
+            className="w-[calc(100%-16px)]"
             label="Повторите пароль"
             type="password"
             placeholder="******"
